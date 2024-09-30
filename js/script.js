5ea6227d29d8ec1b309040b2b0ae8fede8c913c5
Your Name <sumampouw.2912@gmail.com>
2024-09-30 21:34:36 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// icon menu bar
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+
+
+menuIcon.addEventListener("click", function () {
+    menuList.classList.toggle("hidden");
+       
+    });
+    // icon menu bar end
+
+    // form js
+    
+
+    // form js end
